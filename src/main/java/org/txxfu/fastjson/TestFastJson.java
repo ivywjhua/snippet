419186612bdc98dfdcf17d9f72d1080faa430583
Jinhua Wang <ivywjhua@gmail.com>
2012-02-09 17:49:42 +0800

    test fast json

---
+++ src/main/java/org/txxfu/fastjson/TestFastJson.java
@@ -0,0 +1,34 @@
+package org.txxfu.fastjson;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.parser.Feature;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+
+public class TestFastJson {
+
+	public static void main(String[] args) throws IOException {
+//		testEnumAndPos();
+		String str = FileUtils.readFileToString(new File("data/json/json1"));
+		Object json = JSON.parse(str);
+		System.out.println(json);
+		
+	}
+
+	private static void testEnumAndPos() {
+		System.out.println("feature size: " + Feature.values().length);
+		System.out.println("SerializerFeature size: "
+				+ SerializerFeature.values().length);
+		for (Feature f : Feature.values()) {
+			System.out.println(f.ordinal() + "\t" + f.getMask());
+		}
+
+		for (int i = 0; i < 16; i++) {
+			System.out.println(1 << i);
+		}
+	}
+}
