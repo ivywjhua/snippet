4d6c7e62f22f19fad2c1667d02e8bd34c129ca3b
Jinhua Wang <ivywjhua@gmail.com>
2012-03-19 17:59:43 +0800

    add ds

--- src/main/java/org/txxfu/algo/sort/QuickSort2.java
+++ src/main/java/org/txxfu/algo/sort/QuickSort2.java
@@ -1,6 +1,8 @@
 package org.txxfu.algo.sort;
 
-public class QuickSort {
+import java.util.Random;
+
+public class QuickSort2 {
 
 	static void swap(int[] arr, int p1, int p2) {
 		int temp = arr[p1];
@@ -9,29 +11,39 @@ public class QuickSort {
 	}
 
 	static void quickSort(int[] arr, int left, int right) {
-		if(left>=right){
+		if (left >= right) {
 			return;
 		}
-		
-		int m = left;
-		for (int i = left + 1; i < right; i++) {
-			if (arr[i] < arr[left]) {
-				swap(arr, ++m, i);
-			}
-		}
-		swap(arr, left, m);
+		swap(arr, left, randint(left, right));
+		int t = arr[left];
+		int i = left;
+		int j = right + 1;
+		for (;;) {
+			do
+				i++;
+			while (i <= right && arr[i] < t );
 
-		quickSort(arr, left, m - 1);
-		quickSort(arr, m + 1, right);
+			do
+				j--;
+			while (arr[j] > t);
+
+			if (i > j) {
+				break;
+			}
+			swap(arr, i, j);
+		}
+		swap(arr, left, j);
+		quickSort(arr, left, j - 1);
+		quickSort(arr, j + 1, right);
 
 	}
 
 	public static void main(String[] args) {
 		int[] arr = new int[] { 41, 26, 53, 55, 59, 58, 97, 93 };
 		printArr(arr);
-		quickSort(arr, 0, arr.length);
+		quickSort(arr, 0, arr.length - 1);
 		printArr(arr);
-		
+
 	}
 
 	private static void printArr(int[] arr) {
@@ -40,6 +52,11 @@ public class QuickSort {
 			sb.append(arr[i] + " ");
 		}
 		System.out.println(sb.toString());
+	}
+	
+	private static int randint(int left, int right){
+		int z = right - left;
+		return left + new Random().nextInt(z);
 	}
 
 }
